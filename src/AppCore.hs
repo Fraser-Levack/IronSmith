4d46cfe2de5cb4bb91162aed4821fcbc129537eb
Fraser Levack <flevack28@gmail.com>
2026-04-06 20:29:37 +0100

    fix: saving output.glsl globally

--- src/AppCore.hs
+++ src/AppCore.hs
@@ -5,7 +5,6 @@ import System.FilePath ((</>))
 import Data.List (nub)
 import qualified Data.List.NonEmpty as NE
 
--- FIX: We import the core Megaparsec module and just the Position utilities
 import Text.Megaparsec
 import Text.Megaparsec.Pos (sourceLine, unPos) 
 
@@ -14,11 +13,25 @@ import Parser
 import Evaluator
 
 -- | PERSISTENCE HELPERS
-getCachePath :: IO FilePath
-getCachePath = do
+
+-- 1. Grab the global OS folder and make sure it exists
+getConfigDir :: IO FilePath
+getConfigDir = do
     configDir <- getAppUserDataDirectory "ironsmith"
     createDirectoryIfMissing True configDir 
-    return (configDir </> ".ironsmith_recents")
+    return configDir
+
+-- 2. Path for the recent files cache
+getCachePath :: IO FilePath
+getCachePath = do
+    dir <- getConfigDir
+    return (dir </> ".ironsmith_recents")
+
+-- 3. Path for the hidden GLSL output
+getGlslPath :: IO FilePath
+getGlslPath = do
+    dir <- getConfigDir
+    return (dir </> "output.glsl")
 
 loadRecents :: IO [FilePath]
 loadRecents = do
@@ -41,16 +54,17 @@ compileAndSave code =
     case parse pScript "editor" code of
         Left bundle -> do
             let errStr = errorBundlePretty bundle
-                -- 1. Grab the first error in the bundle
                 firstErr = NE.head (bundleErrors bundle)
-                -- 2. Fast-forward the parser state to exactly where the error happened
                 (_, posState) = reachOffset (errorOffset firstErr) (bundlePosState bundle)
-                -- 3. Extract the true line number
                 lineNum = unPos (sourceLine (pstateSourcePos posState))
                 
             return $ Just (errStr, lineNum)
             
         Right astScript -> do
             let glslData = compileToGLSL astScript
-            writeFile "output.glsl" glslData
+            
+            -- FIX: Save the GLSL to the hidden global config folder instead of CWD!
+            glslPath <- getGlslPath
+            writeFile glslPath glslData
+            
             return Nothing
